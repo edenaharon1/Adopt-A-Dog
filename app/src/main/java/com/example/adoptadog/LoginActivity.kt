197f71e03ef4065ee1b6a614a522753cfacabfde
sapirindig <sapirindig@email.com>
2025-02-25 16:27:07 +0200

    drawable and login activity

---
+++ app/src/main/java/com/example/adoptadog/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.adoptadog
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.adoptadog.R
+
+
+class LoginActivity : AppCompatActivity()
+ {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
